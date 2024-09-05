24364a09c549506a532050d41cb0b75617967c48
fraanmm <fr.ortegau@duocuc.cl>
2024-09-05 00:41:28 -0400

    validacion login

--- registro/src/app/login/login.page.ts
+++ registro/src/app/login/login.page.ts
@@ -5,6 +5,8 @@ import{
   Validators,
   FormBuilder
 } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +17,7 @@ export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
 
-  constructor(public fb: FormBuilder ) {
+  constructor(public fb: FormBuilder, public alertController: AlertController ) {
     this.formularioLogin = this.fb.group({
       'nombre': new FormControl("",Validators.required),
       'password': new FormControl("",Validators.required),
@@ -24,6 +26,23 @@ export class LoginPage implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario'));
+
+    if(usuario.nombre == f.nombre && usuario.password == f.password){
+      this.router.navigate(['/home']); 
+    }else{
+      const alert = await this.alertController.create({
+        header:'Datos Incorrectos',
+        message:'El usuario o la contraseña no son correctas',
+        buttons:['Aceptar']
+      });
+
+    }
   }
 
 }

ce8076957817570ed29aca1d950bb21cfe9d67ed
fraanmm <fr.ortegau@duocuc.cl>
2024-09-06 18:56:48 -0400

    actualizacion principal y recuperar

--- registro/src/app/principal/principal.page.ts
+++ registro/src/app/principal/principal.page.ts
@@ -20,4 +20,7 @@ export class PrincipalPage implements OnInit {
   }
 
   ngOnInit() {}
+
+
+  
 }
--- registro/src/app/recuperar/recuperar.page.ts
+++ registro/src/app/recuperar/recuperar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router'; 
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recuperar',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarPage implements OnInit {
 
-  constructor() { }
+  usuario = ''; 
 
-  ngOnInit() {
+  constructor(private router: Router, private alertController: AlertController) { }
+
+  ngOnInit() {}
+
+  async recuperarContra() {
+    if (this.usuario) {
+      
+      const alert = await this.alertController.create({
+        header: '¡Recuperación de contraseña!',
+        message: 'La contraseña ha sido enviada a su correo y reestablecida.',
+        buttons: [
+          {
+            text: 'OK',
+            handler: () => {
+              
+              let navigationExtras: NavigationExtras = {
+                state: {
+                  usuario: this.usuario
+                }
+              };
+
+              
+              this.router.navigate(['/principal'], navigationExtras);
+            }
+          }
+        ]
+      });
+
+     
+      await alert.present();
+    } else {
+      
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, ingrese su nombre de usuario.',
+        buttons: ['OK']
+      });
+
+    
+      await alert.present();
+    }
   }
-
 }
